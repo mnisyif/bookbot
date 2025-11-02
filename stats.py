927feb2316cc86ec777d0b68acc39a6b01e8a6fe
Murtadha <mnisyif@gmail.com>
2025-11-02 18:03:31 -0500

    new: implement get_sort_chars_count
    This method generates a list of dicts with each dict holding a char and
    its count occurance

--- stats.py
+++ stats.py
@@ -15,3 +15,16 @@ def get_chars_count(book_content: str):
             continue
         chars_dict[char.lower()] += 1
     return chars_dict
+
+def sort_on(items):
+    return items["num"]
+
+def get_sorted_chars_count(chars_count: dict):
+    list_chars_count = []
+
+    for item in chars_count:
+        new_dict = {"char": item, "num": chars_count[item]}
+        list_chars_count.append(new_dict)
+    
+    list_chars_count.sort(reverse=True, key=sort_on)
+    return list_chars_count
