51ed3ec80d6d4acae689504ba949b77a1ffe5410
Murtadha <mnisyif@gmail.com>
2025-11-02 17:36:19 -0500

    new: implement get_chars_count and used it in main

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from stats import get_book_text, get_words_count 
+from stats import get_book_text, get_words_count, get_chars_count 
 
 def main():
     book_content = get_book_text("./books/frankenstein.txt")
     words_count = get_words_count(book_content)
+    chars_count = get_chars_count(book_content)
     print(f"Found {words_count} total words")
+    print(chars_count)
 
 main()
--- stats.py
+++ stats.py
@@ -7,4 +7,11 @@ def get_words_count(book_content: str):
     words = book_content.split()
     return len(words)
 
-
+def get_chars_count(book_content: str):
+    chars_dict:dict[str, int] = {}
+    for char in book_content:
+        if char.lower() not in chars_dict:
+            chars_dict[char.lower()] = 1
+            continue
+        chars_dict[char.lower()] += 1
+    return chars_dict
